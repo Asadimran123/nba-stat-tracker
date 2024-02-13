2ff79780a46c73ac593f24729638f90d0f438145
Asadimran123 <asadimran21@gmail.com>
2024-02-12 21:51:41 -0800

    finished layout and added card components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Card from './components/Card'
+
 export default function App() {
 
   return (
     <main>
       <Navbar/>
+      <select id="menu-select">
+        <option value="Teams">Teams</option>
+        <option value="Players">Players</option>
+        <option value="My Teams">My Teams</option>
+        <option value="My Players">My Players</option>
+      </select>
+      <div id="container">
+        <Card
+        name= "asad"
+        />
+      </div>
     </main>
   )
 }
