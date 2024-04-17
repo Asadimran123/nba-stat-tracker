2015b738b97baa493ca1ee9a5ec9e417e432debb
Asadimran123 <asadimran21@gmail.com>
2024-04-17 13:07:32 -0700

    added notes to home

--- src/assets/pages/Home.tsx
+++ src/assets/pages/Home.tsx
@@ -9,6 +9,7 @@ export default function Home(){
             <h1 className="home-header">Welcome!</h1>
             <p className="home-p"> Check out your favorite NBA teams using this NBA stat tracker</p>
             <h3 className="home-header">Future plans</h3>
+            <p className="home-p"> Configure deployment to load logo and fonts for header</p>
             <p className="home-p"> Mobile view optimization</p>
             <p className="home-p"> Player search and favorite functionality</p>
             <p className="home-p"> Live game scores</p>
