23abf2b8719ee976506437c51bbde40bcbacd65f
Asadimran123 <asadimran21@gmail.com>
2024-04-25 14:34:22 -0700

    refactored navbar into dropdown menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,17 +6,40 @@ import Logo from "../assets/134-1349206_nba-logo-png-transparent-background-nba-
 import "../App.css"
 import "../assets/esp/esp_bdit.ttf"
 
-export default function Navbar(){
+export default function Navbar() {
+    const [showMenu, setShowMenu] = React.useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+
     return (
         <nav id="nav">
-            <img id="nba-logo" src={Logo}alt="nba-logo" />
+            <img id="nba-logo" src={Logo} alt="nba-logo" />
             <h1 id="nav-header">NBA stat tracker</h1>
-            <Link to="/" className="nav-btn">Home</Link>
-            <Link to="/Teams" className="nav-btn">Teams</Link>
-            <Link to="/Players" className="nav-btn">Players</Link>
-            <Link to="/MyTeams" className="nav-btn">My Teams</Link>
-            <Link to="/About" className="nav-btn">About</Link>
-            <Link to="/Contact" className="nav-btn">Contact</Link>
+            <button className="menu-btn" onClick={toggleMenu}>
+                Toggle Menu
+            </button>
+            <div className={`nav-links ${showMenu ? 'show' : ''}`}>
+                <Link to="/" className="nav-btn" onClick={toggleMenu}>
+                    Home
+                </Link>
+                <Link to="/Teams" className="nav-btn" onClick={toggleMenu}>
+                    Teams
+                </Link>
+                <Link to="/Players" className="nav-btn" onClick={toggleMenu}>
+                    Players
+                </Link>
+                <Link to="/MyTeams" className="nav-btn" onClick={toggleMenu}>
+                    My Teams
+                </Link>
+                <Link to="/About" className="nav-btn" onClick={toggleMenu}>
+                    About
+                </Link>
+                <Link to="/Contact" className="nav-btn" onClick={toggleMenu}>
+                    Contact
+                </Link>
+            </div>
         </nav>
-    )
+    );
 }
